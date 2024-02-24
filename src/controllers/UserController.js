8259210aaab8a8525d0ef9b600823a4e9b2a1456
Six <igorcesar.dev@gmail.com>
2024-02-23 22:51:56 -0300

    Criação do CRUD de usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,81 @@ class UserController { // Definição da classe UserController;
   async create(req, res) { // Método index que lida com as requisições HTTP;
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id); // Busca usuário pelo ID;
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id); // Busca usuário pelo ID;
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,14 +3,9 @@ import userController from '../controllers/UserController';
 
 const router = new Router();
 
-router.post('/', userController.create);
-
+router.post('/', userController.create); // create/store -> Criar novo usuário -> POST
+router.get('/', userController.index); // index -> Lista todos usuários -> GET
+router.get('/:id', userController.show); // show -> Mostra um usuário -> GET
+router.put('/:id', userController.update); // update -> Atualiza um usuário -> PATCH ou PUT
+router.delete('/:id', userController.delete); // delete -> Apaga um usuário -> DELETE
 export default router;
-
-/*
-index -> Lista todos usuários -> GET
-create/store -> Criar novo usuário -> POST
-delete -> Apaga um usuário -> DELETE
-show -> Mostra um usuário -> GET
-update -> Atualiza um usuário -> PATCH ou PUT
-*/
